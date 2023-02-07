ae57366ab098520cce56026bc33cd7721b11db74
Amir Mardani <amir.mardani@husqvarnagroup.com>
2023-02-07 11:37:47 +0100

    C Def file Updated

--- C_Def.c
+++ C_Def.c
@@ -1,1 +1,43 @@
 #C instruction
+#Void pointer:
+/*
+please read below example about pointers:
+
+*/
+
+#Volatile Variable:
+/* 
+Introduction:
+The Volatile used to inform the compiler
+that the variable value can be changed any
+time without any task given by the source code.
+
+*/
+
+/* 
+Syntax:
+volatile data_type variable_name;
+volatile data_type *variable_name;
+*/
+
+/* Example:*/
+volatile int x;
+volatile int *a;
+
+#Interrupts (latancy, process,...)
+/*
+please read below example about Interrupts:
+
+*/
+
+#Address mapping
+/*
+please read below example about Address maping:
+
+*/
+
+#Structure size
+/*
+please read below example about Structure size:
+
+*/
