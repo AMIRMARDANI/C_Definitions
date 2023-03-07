a65fe4fdb092aa36a0c4a2c366419c9f63916d47
Amir Mardani (external) <Amir.Mardani@husqvarnagroup.com>
2023-03-07 13:31:28 +0100

    C_Def updated 20230307

--- C_Def.c
+++ C_Def.c
@@ -760,6 +760,30 @@ printf("\nAfter swap a=%d b=%d,a,b);
 getch();
 }
 
+/*
+25-print Fibonancci series without using recursion:
+Example:
+*/
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int n1=0,n2=1,n3,i,number;
+	clrscr();
+	printf("Enter the number of elements:);
+	scanf("%d",&number);
+	printf("\n%d %d",n1,n2);//printing 0 and 1
+	
+	for (i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed
+	{
+		n3 = n1+ n2;
+		printf("%d",n3);
+		n1=n2;
+		n2=n3;
+	}
+	getch();
+}
+
 #Address mapping
 /*
 please read below example about Address maping:
